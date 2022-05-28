c97a4b52dbb9676ef803b9e8ff72b7a6901f020c
Yohanes Adrian Sugiono (s3764954) <80510600+yohanesadrian@users.noreply.github.com>
2022-05-28 17:42:13 +1000

    add swagger

--- P000056CSITCP/src/main/java/com/example/project000056/Main.java
+++ P000056CSITCP/src/main/java/com/example/project000056/Main.java
@@ -4,6 +4,7 @@ import com.example.project000056.qrcode.QRCodeGenerator;
 import com.google.zxing.WriterException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
--- P000056CSITCP/src/main/java/com/example/project000056/security/WebSecurityConfig.java
+++ P000056CSITCP/src/main/java/com/example/project000056/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -68,6 +69,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return source;
   }
 
+  private static final String[] AUTH_WHITELIST = {
+          // -- Swagger UI v2
+          "/v2/api-docs",
+          "/swagger-resources",
+          "/swagger-resources/**",
+          "/configuration/ui",
+          "/configuration/security",
+          "/swagger-ui.html",
+          "/webjars/**",
+          // -- Swagger UI v3 (OpenAPI)
+          "/v3/api-docs/**",
+          "/swagger-ui/**"
+  };
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -75,9 +89,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
       .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-      .antMatchers("/api/test/**").permitAll()
+      .antMatchers("/api/test/**").permitAll().antMatchers(AUTH_WHITELIST).permitAll()
       .anyRequest().authenticated();
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
   }
+
 }
